918f9521a628474e6422a72060cf4af4fd08f8e9
Janek Zieleźnicki <janek.zieleznicki@gmail.com>
2024-12-04 08:52:34 +0100

    cleanup

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,6 +1,6 @@
 #![feature(unsigned_signed_diff)]
 
-use std::cmp::{max, min};
+use std::cmp::min;
 use std::collections::HashSet;
 use std::fmt::Debug;
 use std::fs::File;
@@ -13,8 +13,8 @@ fn main() {
     example_data.push("resources/puzzle-input.txt");
     let data: Vec<Vec<i16>> = parse(&mut BufReader::new(File::open(example_data).unwrap()));
     let with_dempener = log_input(&data);
-    println!("Safe reports: {}, expected 306",safe_reports(&data));
-    println!("With Dampener: {}, expected 366",with_dempener);
+    println!("Safe reports: {}, expected 306", safe_reports(&data));
+    println!("With Dampener: {}, expected 366", with_dempener);
 }
 
 pub fn parse<Reader, Type>(reader: &mut Reader) -> Vec<Vec<Type>>
@@ -33,28 +33,25 @@ where
         })
         .collect()
 }
-fn is_safe_report(input: &[i16]) -> bool
-{
+fn is_safe_report(input: &[i16]) -> bool {
     let mut signum = 0;
-    for diff in input.windows(2)
-        .map(|w| w[0]-w[1]) {
-        if signum == 0{signum = diff.signum()}
-        if signum == diff.signum() && diff.abs() >= 1 && diff.abs() <= 3 {
-            continue
+    for diff in input.windows(2).map(|w| w[0] - w[1]) {
+        if signum == 0 {
+            signum = diff.signum()
         }
-        return false
+        if signum == diff.signum() && diff.abs() >= 1 && diff.abs() <= 3 {
+            continue;
+        }
+        return false;
     }
     true
 }
 fn unsafe_level(input: &[i16]) -> Option<usize> {
     let mut signums = HashSet::new();
-    for (idx,diff) in input.windows(2)
-        .map(|w| w[1]-w[0]).enumerate() {
+    for (idx, diff) in input.windows(2).map(|w| w[1] - w[0]).enumerate() {
         signums.insert(diff.signum());
-        if (signums.contains(&1) && signums.contains(&-1)) ||
-            1 > diff.abs() ||
-            diff.abs() > 3 {
-            return Some(idx)
+        if (signums.contains(&1) && signums.contains(&-1)) || 1 > diff.abs() || diff.abs() > 3 {
+            return Some(idx);
         }
     }
     None
@@ -64,13 +61,17 @@ fn safe_with_dampener(values: &[i16]) -> bool {
     match unsafe_level(values) {
         None => true,
         Some(idx) => {
-            (idx.checked_sub(1).unwrap_or(0)..min(idx+1, values.len()-1)+1)
-            .any(|idx|{
-                let vals = values.iter().enumerate()
+            (idx.checked_sub(1).unwrap_or(0)..min(idx + 1, values.len() - 1) + 1).any(|idx| {
+                let vals = values
+                    .iter()
+                    .enumerate()
                     .filter_map(|(i, val)| {
-                        if i==idx { return None }
+                        if i == idx {
+                            return None;
+                        }
                         Some(*val)
-                    }).collect::<Vec<_>>();
+                    })
+                    .collect::<Vec<_>>();
                 unsafe_level(&vals).is_none()
             })
         }
@@ -80,7 +81,11 @@ fn safe_with_dampener(values: &[i16]) -> bool {
 fn log_input(vals: &Vec<Vec<i16>>) -> usize {
     for row in vals {
         let safe = safe_with_dampener(&row);
-        println!("{} -> {safe}", row.iter().fold("".to_string(), |out, val| format!("{out} {val}")))
+        println!(
+            "{} -> {safe}",
+            row.iter()
+                .fold("".to_string(), |out, val| format!("{out} {val}"))
+        )
     }
     vals.iter().filter(|row| safe_with_dampener(row)).count()
 }
@@ -89,7 +94,7 @@ pub fn safe_reports(input: &Vec<Vec<i16>>) -> usize {
 }
 #[cfg(test)]
 mod tests {
-    use crate::{safe_with_dampener, parse, safe_reports};
+    use crate::{parse, safe_reports, safe_with_dampener};
     use std::fs::File;
     use std::io::BufReader;
     use std::path::PathBuf;
@@ -98,16 +103,18 @@ mod tests {
     fn test_safe_reports() {
         let mut example_data = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
         example_data.push("resources/example-input.txt");
-        let  data: Vec<Vec<i16>> = parse(&mut BufReader::new(File::open(example_data).unwrap()));
-        assert_eq!(2,safe_reports(&data));
+        let data: Vec<Vec<i16>> = parse(&mut BufReader::new(File::open(example_data).unwrap()));
+        assert_eq!(2, safe_reports(&data));
     }
     #[test]
     fn test_dampened() {
         let mut example_data = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
         example_data.push("resources/example-input.txt");
         let data: Vec<Vec<i16>> = parse(&mut BufReader::new(File::open(example_data).unwrap()));
-        let with_dempener =
-            data.iter().filter(|row| safe_with_dampener(row.as_slice())).count();
-        assert_eq!(4,with_dempener);
+        let with_dempener = data
+            .iter()
+            .filter(|row| safe_with_dampener(row.as_slice()))
+            .count();
+        assert_eq!(4, with_dempener);
     }
 }
--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,31 +1,25 @@
 #![feature(unsigned_signed_diff)]
 
-use std::cmp::{max, min};
-use std::collections::HashSet;
-use std::fmt::Debug;
-use std::fs::File;
-use std::io::{BufRead, BufReader, Read};
-use std::path::PathBuf;
-use std::str::FromStr;
 use regex::Regex;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::PathBuf;
 
 fn main() {
     let mut puzzle = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     puzzle.push("resources/puzzle-input.txt");
     let calculated = multiplier(&mut BufReader::new(File::open(puzzle.clone()).unwrap()));
     let part2 = multiplier_part2(&mut BufReader::new(File::open(puzzle).unwrap()));
-    println!("Calculated: {}",calculated);
-    println!("Conditional: {}",part2);
+    println!("Calculated: {}", calculated);
+    println!("Conditional: {}", part2);
 }
 pub fn multiplier<Reader>(reader: &mut Reader) -> i64
 where
     Reader: ?Sized + BufRead,
 {
     let mut buffer = String::new();
-    match reader.read_to_string(&mut buffer){
-        Ok(_) => {
-            compute(&buffer)
-        },
+    match reader.read_to_string(&mut buffer) {
+        Ok(_) => compute(&buffer),
         Err(e) => panic!("{}", e),
     }
 }
@@ -34,7 +28,10 @@ fn compute(buffer: &str) -> i64 {
     let reg = Regex::new(r"mul\((?<arg1>[[:digit:]]+),(?<arg2>[[:digit:]]+)\)").unwrap();
     reg.captures_iter(&buffer)
         .map(|caps| {
-            (caps.name("arg1").unwrap().as_str().parse::<i64>().unwrap(), caps.name("arg2").unwrap().as_str().parse::<i64>().unwrap())
+            (
+                caps.name("arg1").unwrap().as_str().parse::<i64>().unwrap(),
+                caps.name("arg2").unwrap().as_str().parse::<i64>().unwrap(),
+            )
         })
         .map(|(arg1, arg2)| arg1 * arg2)
         .sum()
@@ -45,34 +42,41 @@ where
     Reader: ?Sized + BufRead,
 {
     let mut buffer = String::new();
-    match reader.read_to_string(&mut buffer){
+    match reader.read_to_string(&mut buffer) {
         Err(e) => panic!("{}", e),
         Ok(_) => {}
     }
     {
-        let cleared = Regex::new(r"(?:don't\(\))([\s\S]+?)(?:do\(\))").unwrap().replace_all(&buffer, "");
+        let cleared = Regex::new(r"(?:don't\(\))([\s\S]+?)(?:do\(\))")
+            .unwrap()
+            .replace_all(&buffer, "");
         dbg!(&cleared);
         return compute(&cleared);
     }
-    todo!()
 }
 #[cfg(test)]
 mod tests {
+    use crate::{multiplier, multiplier_part2};
     use std::fs::File;
     use std::io::BufReader;
     use std::path::PathBuf;
-    use crate::{multiplier, multiplier_part2};
 
     #[test]
     fn test_part1() {
         let mut example_data = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
         example_data.push("resources/example-input.txt");
-        assert_eq!(161, multiplier(&mut BufReader::new(File::open(example_data).unwrap())));
+        assert_eq!(
+            161,
+            multiplier(&mut BufReader::new(File::open(example_data).unwrap()))
+        );
     }
     #[test]
     fn test_part2() {
         let mut example_data = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
         example_data.push("resources/example-input2.txt");
-        assert_eq!(48, multiplier_part2(&mut BufReader::new(File::open(example_data).unwrap())));
+        assert_eq!(
+            48,
+            multiplier_part2(&mut BufReader::new(File::open(example_data).unwrap()))
+        );
     }
 }
