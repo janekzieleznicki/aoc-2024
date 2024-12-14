a332c7b9c61c0b99eb9efcbc22c9d24bd7ceba5f
Janek Zieleźnicki <janek.zieleznicki@gmail.com>
2024-12-15 00:21:45 +0100

    day13 solution

---
+++ day13/src/main.rs
@@ -0,0 +1,139 @@
+#![feature(unsigned_signed_diff)]
+
+use std::cmp::{min, PartialEq};
+use regex::Regex;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::ops::Add;
+use std::path::PathBuf;
+
+fn main() {
+    let _reader = read_input("puzzle-input.txt");
+    let mut machines = Machines::from(_reader);
+    println!("Used tokens: {}", machines.used_tokens());
+    machines.machines.iter_mut().for_each(|m| {
+        m.prize.x += 10000000000000;
+        m.prize.y += 10000000000000;
+    });
+    println!("Used tokens part 2: {}", machines.used_tokens());
+}
+#[derive(Debug, Ord, PartialOrd, Eq, PartialEq, Copy, Clone)]
+struct Button {
+    x: isize,
+    y: isize,
+}
+type Position = Button;
+impl Add<Button> for Position {
+    type Output = Position;
+    fn add(self, other: Button) -> Position {
+        Position{
+            x: self.x + other.x,
+            y: self.y + other.y
+        }
+    }
+}
+#[derive(Debug, Copy, Clone)]
+struct ClawMachine {
+    button_a: Button,
+    button_b: Button,
+    prize: Position,
+    cost: usize
+}
+
+impl ClawMachine {
+    fn cheapest_combination(&mut self) -> Option<isize> {
+        // solve for B
+        let B = (self.button_a.y*self.prize.x - self.button_a.x*self.prize.y) /
+            (self.button_a.y*self.button_b.x - self.button_a.x*self.button_b.y);
+        // solve for A
+        let A = (self.prize.x - self.button_b.x*B) / self.button_a.x;
+        let end_pos = Position{x: B*self.button_b.x + A*self.button_a.x, y: B*self.button_b.y + A*self.button_a.y};
+        if self.prize == end_pos{
+            return Some(B*1+A*3)
+        }
+        None
+    }
+}
+
+struct Machines {
+    machines: Vec<ClawMachine>,
+}
+impl Machines{
+    pub fn used_tokens(&self) -> isize {
+        self.machines.iter().cloned().filter_map(|mut m| m.cheapest_combination()).sum()
+    }
+}
+impl<Reader> From<Reader> for Machines
+where
+    Reader: BufRead,
+{
+    fn from(mut value: Reader) -> Self {
+        let mut string = String::new();
+        value.read_to_string(&mut string).unwrap();
+        let re = Regex::new(r"(Button A: X\+(?<ax>\d+), Y\+(?<ay>\d+)\nButton B: X\+(?<bx>\d+), Y\+(?<by>\d+)\nPrize: X=(?<px>\d+), Y=(?<py>\d+))").unwrap();
+        Self {
+            machines: re
+                .captures_iter(&string)
+                .map(|cap| ClawMachine {
+                    button_a: Button {
+                        x: cap["ax"].parse().unwrap(),
+                        y: cap["ay"].parse().unwrap(),
+                    },
+                    button_b: Button {
+                        x: cap["bx"].parse().unwrap(),
+                        y: cap["by"].parse().unwrap(),
+                    },
+                    prize: Button {
+                        x: cap["px"].parse::<isize>().unwrap(),
+                        y: cap["py"].parse::<isize>().unwrap(),
+                    },
+                    cost: usize::MAX,
+                })
+                .collect(),
+        }
+    }
+}
+fn read_input(name: &str) -> BufReader<File> {
+    let mut example_data = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    example_data.push(format!("resources/{name}"));
+
+    BufReader::new(File::open(example_data).unwrap())
+}
+#[cfg(test)]
+mod tests {
+
+    use crate::{read_input, Machines};
+
+    #[test]
+    fn test_part1() {
+        let _reader = read_input("example-input.txt");
+        let mut machines = Machines::from(_reader);
+        println!("First machine");
+        assert_eq!(machines.machines[1].cheapest_combination(), None);
+        println!("Second machine");
+        assert_eq!(machines.machines[3].cheapest_combination(), None);
+        println!("Third machine");
+        assert_eq!(machines.machines[0].cheapest_combination(), Some(280));
+        println!("Forth machine");
+        assert_eq!(machines.machines[2].cheapest_combination(), Some(200));
+    }
+    #[test]
+    fn test_part2() {
+        let _reader = read_input("example-input.txt");
+        let _reader = read_input("example-input.txt");
+        let mut machines = Machines::from(_reader);
+        machines.machines.iter_mut().for_each(|m| {
+            m.prize.x += 10000000000000;
+            m.prize.y += 10000000000000;
+        });
+        println!("First machine");
+        assert_eq!(machines.machines[1].cheapest_combination(), Some(459236326669));
+        println!("Second machine");
+        assert_eq!(machines.machines[3].cheapest_combination(), Some(416082282239));
+        println!("Third machine");
+        assert_eq!(machines.machines[0].cheapest_combination(), None);
+        println!("Forth machine");
+        assert_eq!(machines.machines[2].cheapest_combination(), None);
+
+    }
+}
