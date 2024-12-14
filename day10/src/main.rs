b03fc1151df9e8d4751dbb4191743d2f21334797
Janek Zieleźnicki <janek.zieleznicki@gmail.com>
2024-12-14 18:25:05 +0100

    day11 solution

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -13,12 +13,12 @@ fn main() {
     println!("Trailhead rating: {}", topo.trailhead_rating());
 }
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
-struct Position{
+struct Position {
     x: isize,
     y: isize,
 }
-#[derive(Debug,Copy, Clone)]
-enum Direction{
+#[derive(Debug, Copy, Clone)]
+enum Direction {
     Top,
     Bottom,
     Left,
@@ -35,58 +35,79 @@ impl TopoMap {
         self.elevations[pos.y as usize * self.dimensions.x as usize + pos.x as usize]
     }
 
-
     fn can_move(&self, pos: &Position, dir: Direction) -> Option<Position> {
-        let in_dimensions = |new_pos: Position|{
-          if new_pos.x < 0 || new_pos.x >= self.dimensions.x {
-              return None
-          }
+        let in_dimensions = |new_pos: Position| {
+            if new_pos.x < 0 || new_pos.x >= self.dimensions.x {
+                return None;
+            }
             if new_pos.y < 0 || new_pos.y >= self.dimensions.y {
-                return None
+                return None;
             }
             Some(new_pos)
         };
         in_dimensions(match dir {
-            Direction::Top => {Position{x: pos.x, y: pos.y - 1}},
-            Direction::Bottom => {Position{x: pos.x, y: pos.y + 1}},
-            Direction::Left => {Position{x: pos.x - 1, y: pos.y}},
-            Direction::Right => {Position{x: pos.x + 1, y: pos.y}},
+            Direction::Top => Position {
+                x: pos.x,
+                y: pos.y - 1,
+            },
+            Direction::Bottom => Position {
+                x: pos.x,
+                y: pos.y + 1,
+            },
+            Direction::Left => Position {
+                x: pos.x - 1,
+                y: pos.y,
+            },
+            Direction::Right => Position {
+                x: pos.x + 1,
+                y: pos.y,
+            },
         })
     }
     fn reachable_summits(&self, pos: &Position, start_elevation: u8) -> Vec<Position> {
         let mut reachable: Vec<Position> = Vec::new();
-        for new_pos in vec![Direction::Bottom, Direction::Right, Direction::Left, Direction::Top].iter()
-            .filter_map(|dir| self.can_move(pos, *dir)){
-            match self.elevation_at(&new_pos){
-                x if x == 9 && start_elevation == 8=> reachable.push(new_pos),
-                x if x == start_elevation+1 => reachable.append(&mut self.reachable_summits(&new_pos,x)),
+        for new_pos in vec![
+            Direction::Bottom,
+            Direction::Right,
+            Direction::Left,
+            Direction::Top,
+        ]
+        .iter()
+        .filter_map(|dir| self.can_move(pos, *dir))
+        {
+            match self.elevation_at(&new_pos) {
+                x if x == 9 && start_elevation == 8 => reachable.push(new_pos),
+                x if x == start_elevation + 1 => {
+                    reachable.append(&mut self.reachable_summits(&new_pos, x))
+                }
                 _ => {}
             }
         }
         reachable
     }
     pub fn trailhead_scores(&self) -> usize {
-        // for th in self.trailheads.iter(){
-        //     println!("From {th:?} we can reach {}",self.reachable_summits(th,0).into_iter().collect::<HashSet<_>>().len())
-        // }
-        self.trailheads.iter()
-            .map(|th|self.reachable_summits(th,0).into_iter().collect::<HashSet<_>>().len())
+        self.trailheads
+            .iter()
+            .map(|th| {
+                self.reachable_summits(th, 0)
+                    .into_iter()
+                    .collect::<HashSet<_>>()
+                    .len()
+            })
             .sum()
     }
     pub fn trailhead_rating(&self) -> usize {
-        // for th in self.trailheads.iter(){
-        //     println!("From {th:?} we can reach {}",self.reachable_summits(th,0).into_iter().collect::<HashSet<_>>().len())
-        // }
-        self.trailheads.iter()
-            .map(|th|self.reachable_summits(th,0).len())
+        self.trailheads
+            .iter()
+            .map(|th| self.reachable_summits(th, 0).len())
             .sum()
     }
 }
 impl Display for TopoMap {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        for y in 0..self.dimensions.y{
+        for y in 0..self.dimensions.y {
             for x in 0..self.dimensions.x {
-                write!(f, "{}", self.elevation_at(&Position{x,y}))?;
+                write!(f, "{}", self.elevation_at(&Position { x, y }))?;
             }
             write!(f, "\n")?;
         }
@@ -103,23 +124,31 @@ where
         let mut elevations = Vec::new();
         let mut summits = Vec::new();
         let mut trailheads = Vec::new();
-        let mut dimensions = Position{x: 0, y: 0};
-        for (y,line) in reader.lines()
-            .map_while(|line| {line.ok()}).enumerate() {
-            dimensions = Position{x: line.len() as isize, y: y as isize+1};
-            for (x,char) in line.chars().enumerate() {
+        let mut dimensions = Position { x: 0, y: 0 };
+        for (y, line) in reader.lines().map_while(|line| line.ok()).enumerate() {
+            dimensions = Position {
+                x: line.len() as isize,
+                y: y as isize + 1,
+            };
+            for (x, char) in line.chars().enumerate() {
                 elevations.push(char.to_digit(10).unwrap() as u8);
                 match char {
-                    '9' => summits.push(Position{x: x as isize, y: y as isize}),
-                    '0' => trailheads.push(Position{x: x as isize, y:y as isize}),
+                    '9' => summits.push(Position {
+                        x: x as isize,
+                        y: y as isize,
+                    }),
+                    '0' => trailheads.push(Position {
+                        x: x as isize,
+                        y: y as isize,
+                    }),
                     _ => {}
                 }
             }
         }
-        Self{
-             elevations,
-             summits,
-             trailheads,
+        Self {
+            elevations,
+            summits,
+            trailheads,
             dimensions,
         }
     }
@@ -133,7 +162,7 @@ fn read_input(name: &str) -> BufReader<File> {
 #[cfg(test)]
 mod tests {
 
-    use crate::{read_input, TopoMap};
+    use crate::{TopoMap, read_input};
 
     #[test]
     fn test_part1() {
---
+++ day11/src/main.rs
@@ -0,0 +1,141 @@
+#![feature(unsigned_signed_diff)]
+
+use std::collections::{HashMap, LinkedList};
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::PathBuf;
+
+fn main() {
+    let _reader = read_input("puzzle-input.txt");
+    let mut grid = Grid::from(_reader);
+    for _ in 0..25 {
+        grid.blink();
+    }
+    println!("Stone count after 25 blinks: {}", grid.stone_count());
+    for _ in 0..50 {
+        grid.blink();
+    }
+    println!("Stone count after 75 blinks: {}", grid.stone_count());
+}
+#[derive(Ord, PartialOrd, Eq, PartialEq, Hash, Copy, Clone, Debug)]
+struct Stone {
+    number: u128,
+}
+impl Stone {
+    pub fn blink(self) -> (Stone, Option<Stone>) {
+        match self.number {
+            0 => (Stone { number: 1 }, None),
+            x if Self::digits_count(x) % 2 == 0 => {
+                let half_digit = Self::digits_count(x) / 2;
+                (
+                    Stone {
+                        number: x / 10u128.pow(half_digit as u32),
+                    },
+                    Some(Stone {
+                        number: x % 10u128.pow(half_digit as u32),
+                    }),
+                )
+            }
+            x => (Stone { number: x * 2024 }, None),
+        }
+    }
+
+    fn digits_count(num: u128) -> usize {
+        (0..).take_while(|i| 10u128.pow(*i) <= num).count()
+    }
+}
+struct Grid {
+    stones: HashMap<Stone, usize>,
+}
+impl Grid {
+    pub fn blink(&mut self) {
+        let mut new_stones = HashMap::new();
+        for (stone, count) in self.stones.iter() {
+            match stone.blink() {
+                (left, Some(right)) => {
+                    *new_stones.entry(left).or_insert(0) += count;
+                    *new_stones.entry(right).or_insert(0) += count;
+                }
+                (stone, None) => {
+                    *new_stones.entry(stone).or_insert(0) += count;
+                }
+            }
+        }
+        self.stones = new_stones;
+    }
+    pub fn stone_count(&self) -> usize {
+        self.stones.values().sum()
+    }
+}
+impl<Reader> From<Reader> for Grid
+where
+    Reader: BufRead,
+{
+    fn from(mut reader: Reader) -> Self {
+        let mut buf = String::new();
+        reader.read_to_string(&mut buf).unwrap();
+        let mut stones = HashMap::new();
+        buf.split_whitespace()
+            .into_iter()
+            .map(|str| Stone {
+                number: str.parse().unwrap(),
+            })
+            .for_each(|stone| *stones.entry(stone).or_insert(0) += 1);
+        Self { stones }
+    }
+}
+fn read_input(name: &str) -> BufReader<File> {
+    let mut example_data = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    example_data.push(format!("resources/{name}"));
+
+    BufReader::new(File::open(example_data).unwrap())
+}
+#[cfg(test)]
+mod tests {
+    use crate::{Grid, Stone, read_input};
+    use std::io::BufReader;
+    #[test]
+    fn test_helpers() {
+        assert_eq!(Stone::digits_count(11221), 5);
+        assert_eq!(Stone::digits_count(125), 3);
+        assert_eq!(Stone::digits_count(253000), 6);
+        assert_eq!(Stone::digits_count(1000), 4);
+
+        {
+            let stone = Stone { number: 253000 }.blink();
+            assert_eq!(stone, (Stone { number: 253 }, Some(Stone { number: 0 })));
+        }
+        {
+            let stone = Stone { number: 512072 }.blink();
+            assert_eq!(stone, (Stone { number: 512 }, Some(Stone { number: 72 })));
+        }
+        {
+            let stone = Stone { number: 14168 }.blink();
+            assert_eq!(stone, (Stone { number: 28676032 }, None));
+        }
+    }
+    #[test]
+    fn test_part1() {
+        let _reader = read_input("example-input.txt");
+        let mut grid = Grid::from(_reader);
+        assert_eq!(grid.stone_count(), 5);
+        for _ in 0..1 {
+            grid.blink();
+        }
+        assert_eq!(grid.stone_count(), 7);
+        let mut grid = Grid::from("125 17".as_bytes());
+        for _ in 0..6 {
+            grid.blink();
+        }
+        assert_eq!(grid.stone_count(), 22);
+        let mut grid = Grid::from("125 17".as_bytes());
+        for _ in 0..25 {
+            grid.blink();
+        }
+        assert_eq!(grid.stone_count(), 55312);
+    }
+    #[test]
+    fn test_part2() {
+        let _reader = read_input("example-input.txt");
+    }
+}
